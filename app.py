0d91578f9a9b19cd14387d5f11892b10f94b4009
AlassaneASC20 <68346448+AlassaneASC20@users.noreply.github.com>
2021-03-20 13:48:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ if __name__ == '__main__':
     
     @app.route('/')
     def index():
-        return
+        return render_template("index.html")
+
+    @app.route('/success')  
+    def success():
+
+        return render_template("success.html", message = message)
 
 
 
